191a5e2b4f75d371460ad928f33308fcd487125c
Calvin <huynhck001@gmail.com>
2017-10-05 15:02:44 +0200

    rewrote data filter to use read_csv2 (from readr package)

--- PD_data_subsetted.R
+++ PD_data_subsetted.R
@@ -12,8 +12,7 @@ library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("parameters.R")
 
-data <- read.csv(pathToCSV, header=TRUE, sep=";")
-
 # filter data based on 
-PD_filtered_data <-  data[!(!is.na(data$ChargeTimeHours) & data$ChargeTimeHours==""), ]
+data <- read_csv2(pathToCSV)
 
+PD_filtered_data <- data[!(is.na(data$ChargeTimeHours) | data$ChargeTimeHours==""), ]
