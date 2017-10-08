887cc7dc8765a9b5b32b8edaec6be6fe8410466c
aminChihab <amin_chihab@hotmail.com>
2017-10-08 20:59:29 +0200

    Basic stats done

---
+++ src/stations_per_user.R
@@ -0,0 +1,34 @@
+library(readr)
+library(ggplot2)
+library(config)
+library(dplyr)
+config <- config::get()
+source(config$baseClean)
+
+df <- read_csv2(config$scDataset)
+df <- cleanDataframe(df)
+
+#Select the amount of unique sessions per user per charging station 
+uniqueUsersPerStationSubset <- df %>%
+  select(user_id, latitude, longitude) %>%
+  arrange(user_id) %>% 
+  distinct(user_id,longitude,latitude) 
+
+#The amount of users per amount of station used
+#TODO: How is this distributed in percentages
+countOfUsersPerDifferentStations <- uniqueUsersPerStationSubset %>%
+  group_by(user_id) %>%
+  summarise(charging_station_count = n()) %>%
+  group_by(charging_station_count) %>%
+  summarise(user_amount = n())
+
+#Plot countOfUsersPerDifferentStations as bar graph
+plotUsersPerDifferentStations <- ggplot(data=countOfUsersPerDifferentStations,aes(x=charging_station_count , y=user_amount)) + geom_bar(stat="identity")
+plotUsersPerDifferentStations + labs(x = "Amount of charging stations used", y = "Amount of users")
+
+#
+
+
+
+
+
