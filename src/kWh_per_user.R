2f8f6bcf44c1e16b7973f154cc527b21fc818753
Ramon <ramon_gill_8@hotmail.com>
2017-10-13 22:30:31 +0200

    WIP

--- src/kWh_per_user.R
+++ src/kWh_per_user.R
@@ -2,20 +2,47 @@ library(ggplot2)
 library(config)
 library(readr)
 library(dplyr)
-config <- config::get(file = "../config.yml")
+config <- config::get(file = "config.yml")
 source(config$baseClean)
 
 df <- read_csv2(config$scDataset)
 df <- cleanDataframe(df)
 
+# Table functions ---------------------------------------------------------
+
+# Returns a table with total kWh grouped by user
+getKwhPerUser <- function() {
+  df %>%
+    group_by(user_id) %>%
+    filter(!is.na(charged_kwh), !is.na(end_date)) %>%
+    summarise(total_charged = sum(charged_kwh))
+}
+
 # Per user total amount charged kwh
-kwhPerUser <- df %>%
-  group_by(user_id) %>%
-  filter(!is.na(charged_kwh)) %>%
-  summarise(total_charged = sum(charged_kwh), 
-            total_hours = sum(hours_elapsed), 
-            total_effective_hours = sum(effective_charging_hours))
+getKWhPerUserPerHour <- function() {
+  df %>%
+    filter(!is.na(charged_kwh), !is.na(end_date)) %>%
+    mutate(end_timeframe = paste(
+      paste(hour(floor_date(end_date, "hour")), "00", sep = ":"), 
+      paste(hour(floor_date(end_date, "hour")) + 1, "00", sep = ":") 
+    )) %>%
+    group_by(end_timeframe) %>%
+    summarise(total_charged = sum(charged_kwh))
+}
+
+# Plot functions ----------------------------------------------------------
 
 #Plotje
-plot <- ggplot(kwhPerUser, aes(y = total_charged, x = total_hours)) + geom_point(alpha = 0.3) + geom_smooth()
-plot + labs(x = "Total hours", y = "Total kWh charged")
+kwhPerUser <- function() {
+  p <- ggplot(getKWhPerUserPerHour(), aes(y = total_charged, x = end_timeframe)) + 
+    geom_point(alpha = 0.5) + 
+    geom_smooth() +
+    labs(y = "Total kWh charged", x = "Timeframe") +
+    ggtitle("kWh charged per hour per user")
+  return (p)
+}
+
+# Calls -------------------------------------------------------------------
+
+total <- getKwhPerUser()
+kwhPerUser()
