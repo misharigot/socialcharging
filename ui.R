a41d9955b15ff881969823eb3b9fa8babeb20b57
Amin <amin_chihab@hotmail.com>
2017-10-17 20:16:38 +0200

    Feature/61 charging stations per users (#20)
    * Basic stats done
    
    * git credential test
    
    * Added TODO comments
    
    * comments
    
    * This is it, I'm done
    
    * Actually done, added function calls
    
    * Deleted some non-used lines
    
    * merged with develop and added plot to shiny
    
    * deleted file in wrong branch

--- ui.R
+++ ui.R
@@ -11,7 +11,8 @@ ui <- dashboardPage(
              menuSubItem("Smart vs non-smart charging", tabName = "chart2"),
              menuSubItem("kWh vs charging stations", tabName = "chart3"),
              menuSubItem("Timeframe vs charging sessions", tabName = "chart4"),
-             menuSubItem("Total KwH per station in map", tabName="chart5")
+             menuSubItem("Total KwH per station in map", tabName="chart5"),
+             menuSubItem("How many users use how many stations", tabName="chart6")
     )
   ),
   dashboardBody(
@@ -68,7 +69,13 @@ ui <- dashboardPage(
                   # If not using custom CSS, set height of leafletOutput to a number instead of percent
                   leafletOutput("plot5", width="100%", height="100%")
                 )
+      ),
+      tabItem(tabName = "chart6",
+              fluidRow(
+                box(plotOutput("plot6"), width = 12)
               )
+      )
     )
   )
 )
+
