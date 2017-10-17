05107e358d5ae27348c5256946e7f8a298bd68c0
U-DESKTOP-JMKUVFS\Amin Chihab <amin_chihab@hotmail.com>
2017-10-17 17:58:57 +0200

    This is it, I'm done

--- src/stations_per_user.R
+++ src/stations_per_user.R
@@ -31,22 +31,14 @@ countOfUsersPerAmountOfStationsUsed <- uniqueUsersPerStation %>%
   summarise(user_amount = n())
 
 
-
-#IF user used more than 1 chargingstation -> check distances between charging stations
-#Distributed distances between users (also calculate mean)
-distanceBetweenStationsUsed <- uniqueUsersPerStation %>%
-  filter(uniqueUsersPerStation[!duplicated(uniqueUsersPerStation[c("user_id","latitude","longitude")]), ])
-  
-
-
-
-
-
 # Plot functions ----------------------------------------------------------
 
 
 #Plot countOfStationsUsed as bar graph
-plotUsersPerDifferentStations <- ggplot(data=countOfUsersPerAmountOfStationsUsed,aes(x=charging_station_count , y=user_amount)) + 
-  geom_bar(stat="identity")
+plotUsersPerDifferentStations <- ggplot(data=countOfUsersPerAmountOfStationsUsed,aes(x=charging_stations_used, y=user_amount)) + geom_bar(stat="identity")
 plotUsersPerDifferentStations + labs(x = "Amount of charging stations used", y = "Amount of users")
 ggtitle("Distributed amount of charging stations used per user in Januari")
+
+
+# Calls -------------------------------------------------------------------
+
