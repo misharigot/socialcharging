a12faad8b66ca33ef346623f42b4e7ec277f0a38
U-DESKTOP-JMKUVFS\Amin Chihab <amin_chihab@hotmail.com>
2017-10-17 18:08:52 +0200

    Actually done, added function calls

--- src/stations_per_user.R
+++ src/stations_per_user.R
@@ -17,28 +17,33 @@ df <- cleanDataframe(df)
 
 
 #Select the amount of unique sessions per user per charging station 
-uniqueUsersPerStation <- df %>%
+uniqueUsersPerStation <- function(){ 
+  df %>%
   select(user_id, latitude, longitude) %>%
   arrange(user_id) %>% 
   distinct(user_id,longitude,latitude) 
+}
 
 #Distributed amount of charging stations used per user in Januari
 #TODO: How is this distributed in percentages? Check per week?
-countOfUsersPerAmountOfStationsUsed <- uniqueUsersPerStation %>%
+countOfUsersPerAmountOfStationsUsed <- function(){
+  uniqueUsersPerStation() %>%
   group_by(user_id) %>%
   summarise(charging_stations_used = n()) %>%
   group_by(charging_stations_used) %>%
   summarise(user_amount = n())
-
+}
 
 # Plot functions ----------------------------------------------------------
 
 
 #Plot countOfStationsUsed as bar graph
-plotUsersPerDifferentStations <- ggplot(data=countOfUsersPerAmountOfStationsUsed,aes(x=charging_stations_used, y=user_amount)) + geom_bar(stat="identity")
-plotUsersPerDifferentStations + labs(x = "Amount of charging stations used", y = "Amount of users")
-ggtitle("Distributed amount of charging stations used per user in Januari")
-
+plotUsersPerDifferentStations <- function(){
+    ggplot(data=countOfUsersPerAmountOfStationsUsed(),aes(x=charging_stations_used, y=user_amount)) + 
+    geom_bar(stat="identity") +
+    labs(x = "Amount of charging stations used", y = "Amount of users") +
+    ggtitle("Distributed amount of charging stations used per user in Januari")
+}
 
 # Calls -------------------------------------------------------------------
-
+plotUsersPerDifferentStations()
