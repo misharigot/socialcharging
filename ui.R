42174b1f29b7543c72e6e2952a7c65fc046cf4e4
CalvinHuynh <huynhck001@gmail.com>
2017-10-18 23:37:43 +0200

    Feature/84 interactive plots (#22)
    * Added interactive element on misha's plot
    
    * reset button added, reactive plots expanded
    
    * lintered 2 scripts
    
    
    Nice!

--- ui.R
+++ ui.R
@@ -11,8 +11,8 @@ ui <- dashboardPage(
              menuSubItem("Smart vs non-smart charging", tabName = "chart2"),
              menuSubItem("kWh vs charging stations", tabName = "chart3"),
              menuSubItem("Timeframe vs charging sessions", tabName = "chart4"),
-             menuSubItem("Total KwH per station in map", tabName="chart5"),
-             menuSubItem("How many users use how many stations", tabName="chart6")
+             menuSubItem("Total KwH per station in map", tabName = "chart5"),
+             menuSubItem("How many users use how many stations", tabName = "chart6")
     )
   ),
   dashboardBody(
@@ -27,9 +27,13 @@ ui <- dashboardPage(
         )
       ),
       tabItem(tabName = "chart1",
-              fluidRow(
-                box(plotOutput("plot1"), width = 12)
-              )
+              fluidRow(box(plotOutput("plot1", height = 400,
+                                      dblclick = "dblclick",
+                                      brush = brushOpts(
+                                        id = "brush",
+                                        resetOnNew = TRUE
+                                      )), width = 12)),
+              actionButton("reset_input", "Reset")
       ),
       tabItem(tabName = "chart2",
               fluidRow(
@@ -45,7 +49,13 @@ ui <- dashboardPage(
                               ))
                 )
               ),
-              fluidRow(box(plotOutput("plot2"), title = "Smart charging ", width = 12))
+              fluidRow(box(plotOutput("plot2", height = 400,
+                                      dblclick = "dblclick",
+                                      brush = brushOpts(
+                                        id = "brush",
+                                        resetOnNew = TRUE
+                                      )), title = "Smart charging ", width = 12)),
+              actionButton("reset_input_1", "Reset")
       ),
       tabItem(tabName = "chart3",
               fluidRow(
@@ -60,14 +70,14 @@ ui <- dashboardPage(
               )
       ),
       tabItem(tabName = "chart5",
-              div(class="outer",
+              div(class = "outer",
                   tags$head(
                     # Include our custom CSS
                     includeCSS("src/styles.css"),
                     includeScript("src/gomap.js")
                   ),
                   # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                  leafletOutput("plot5", width="100%", height="100%")
+                  leafletOutput("plot5", width = "100%", height = "100%")
                 )
       ),
       tabItem(tabName = "chart6",
@@ -78,4 +88,3 @@ ui <- dashboardPage(
     )
   )
 )
-
