e27c66f86b132d48e9dd5d2eb4ccce3e0d9ad9c7
Ramon <ramon_gill_8@hotmail.com>
2017-10-19 03:10:59 +0200

    fixed order y axis timeframe

--- src/timeframe_vs_users.R
+++ src/timeframe_vs_users.R
@@ -9,7 +9,31 @@ source(config$multiplotHelper)
 
 df <- read_csv2(config$scDataset)
 df <- cleanDataframe(df)
-
+timeframeLevels <- c("6:00 7:00",
+                     "7:00 8:00",
+                     "8:00 9:00",
+                     "9:00 10:00",
+                     "10:00 11:00",
+                     "11:00 12:00",
+                     "12:00 13:00",
+                     "13:00 14:00",
+                     "14:00 15:00",
+                     "15:00 16:00",
+                     "16:00 17:00",
+                     "17:00 18:00",
+                     "18:00 19:00",
+                     "19:00 20:00",
+                     "20:00 21:00",
+                     "21:00 22:00",
+                     "22:00 23:00",
+                     "23:00 24:00",
+                     "0:00 1:00",
+                     "1:00 2:00",
+                     "2:00 3:00",
+                     "3:00 4:00",
+                     "4:00 5:00",
+                     "5:00 6:00"
+)
 # Table functions ---------------------------------------------------------
 
 # Returns a table with total kWh grouped by user
@@ -28,6 +52,7 @@ getKwhPerUserStartTime <- function() {
       paste(hour(floor_date(start_date, "hour")), "00", sep = ":"),
       paste(hour(floor_date(start_date, "hour")) + 1, "00", sep = ":")
     )) %>%
+    mutate(start_timeframe = factor(start_timeframe, levels = timeframeLevels)) %>%
     group_by(user_id, start_timeframe) %>%
     summarise(total_charged = sum(charged_kwh), count = n(), mean_kwh = mean(charged_kwh)) %>%
     group_by(start_timeframe) %>%
@@ -43,6 +68,7 @@ getHoursElapsedPerUserStartTime <- function() {
       paste(hour(floor_date(start_date, "hour")), "00", sep = ":"), 
       paste(hour(floor_date(start_date, "hour")) + 1, "00", sep = ":") 
     )) %>%
+    mutate(start_timeframe = factor(start_timeframe, levels = timeframeLevels)) %>%
     group_by(user_id, start_timeframe) %>%
     summarise(total_hours_elapsed = sum(hours_elapsed), count = n(), mean_hours = mean(hours_elapsed)) %>%
     group_by(start_timeframe) %>%
@@ -56,6 +82,7 @@ plotKwhPerUserStartTime <- function() {
   p <- ggplot(getKwhPerUserStartTime(), aes(y = m_kwh, x = start_timeframe)) +
     geom_bar(stat = "identity", alpha = 0.5) +
     geom_smooth() +
+    scale_x_discrete(limits = c(timeframeLevels)) +
     labs(y = "Average kwh charged", x = "Start Timeframe") +
     ggtitle("Average of charged kwh at a Timeframe") +
     coord_flip() +
@@ -68,6 +95,7 @@ plotHoursElapsedPerUserEndTime <- function() {
   p <- ggplot(getHoursElapsedPerUserStartTime(), aes(y = m_hours, x = start_timeframe)) +
     geom_bar(stat = "identity", alpha = 0.5) +
     geom_smooth() +
+    scale_x_discrete(limits = c(timeframeLevels)) +
     labs(y = "Average hours elapsed", x = "Start Timeframe") +
     ggtitle("Average hours elapsed at a Timeframe") +
     coord_flip() +
