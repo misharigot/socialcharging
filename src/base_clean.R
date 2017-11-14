4c5306e33b6c196dfd1772b25d4eada37d95e660
sanghunBaek <bsh56592@gmail.com>
2017-11-14 23:02:35 +0100

    updata base_clean.R

--- src/base_clean.R
+++ src/base_clean.R
@@ -31,7 +31,7 @@ cleanSecondDf <- function(df) {
   df %>%
     mutate(start_date = ymd_hms(start_date),
            end_date = ymd_hms(end_date),
-           charged_kwh = as.numeric(charged_kwh) %>%
+           charged_kwh = as.numeric(charged_kwh),
            kw_charge_point_speed = kw_charge_point_speed*0.01) %>%
     mutate(hours_elapsed = sapply(end_date - start_date, function(x) {
       round(x / 3600, 2)
