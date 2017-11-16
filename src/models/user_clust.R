ed12a6b887801f3367cd71f666e7f2d9e1fd2991
misharigot <misharigot@gmail.com>
2017-11-16 01:28:04 +0100

    WIP, refactor of user_class is done. Need top timeframe class in user_clust.R now

--- src/models/user_clust.R
+++ src/models/user_clust.R
@@ -5,10 +5,13 @@ library(plotly)
 library(clValid)
 config <- config::get(file = "config.yml")
 source(config$baseClean)
+source("src/models/user_class.R")
 set.seed(100)
 
 df <- read_csv2(config$scDataset, col_names = FALSE)
 df <- cleanSecondDf(df)
+
+abc <- df %>% filter(!is.na(start_date), !is.na(end_date)) %>% sessionClassificationDf()
 
 # Data cleaning -----------------------------------------------------------------------------------------------
 cleanDataChargedKwh <- function(data) {
@@ -23,24 +26,40 @@ cleanDataChargedKwh <- function(data) {
 
 # Todo: change logic
 cleanDataTime <- function(data) {
-  data %>%
+  topUsersClasses <- userClassificationDf(data)
+  
+  summary <- data %>%
     filter(!is.na(start_date), !is.na(end_date), !is.na(hours_elapsed), hours_elapsed < 56) %>%
     group_by(user_id) %>%
     summarise(avg_hours_elapsed = mean(hours_elapsed),
-              start_time = mean(as.numeric(format(round(start_date, "hours"), format = "%H"))),
-              end_time = mean(as.numeric(format(round(end_date, "hours"), format = "%H"))),
+              # start_time = mean(as.numeric(format(round(start_date, "hours"), format = "%H"))),
+              # end_time = mean(as.numeric(format(round(end_date, "hours"), format = "%H"))),
               n_diff_stations = n_distinct(address)) %>%
-    select(avg_hours_elapsed, start_time, end_time, n_diff_stations)
+    select(avg_hours_elapsed, n_diff_stations)
+  print(str(summary))
+  print(str(topUsersClasses))
+  print(summary(summary))
+  print(summary(topUsersClasses))
+  
+  
+  summary$class <- map(topUsersClasses, function(x) {
+    print(x["user_id"])
+    print("en die andere:")
+    print(summary$user_id)
+    if (x["user_id"] == summary$user_id) {
+      summary$top_class <- x["class"]
+    }
+  })
 }
 
 cleanDfCharged <- cleanDataChargedKwh(df)
-cleanDfTime <- cleanDataTime(df)
+cleanDfTime <- cleanDataTime(abc)
 
 # Kmeans Clustering -------------------------------------------------------------------------------------------
 
 clusteredChargedKm <- kmeans(cleanDfCharged, 5, nstart = 20)
 clusteredTimeKm <- kmeans(cleanDfTime, 5, nstart = 20)
-print(clusteredTimeKm)
+
 # Dunn's Index ------------------------------------------------------------------------------------------------
 
 dunnKmCharged <- dunn(clusters = clusteredChargedKm$cluster, Data = cleanDfCharged)
