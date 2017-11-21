952126843147c75472aa287d19e9d6b4f9f3fcf7
sanghunBaek <bsh56592@gmail.com>
2017-11-21 11:52:41 +0100

    improve it to visulizated at shiny

--- src/plots/visulization_statistic_data.R
+++ src/plots/visulization_statistic_data.R
@@ -11,9 +11,6 @@ library(plotly)
 config <- config::get(file = "config.yml")
 source(config$baseClean)
 
-df <- read_csv2(config$scDataset)
-df <- cleanSecondDf(df)
-
 makeTable <- function(testDf){
   name <- c("total","useable")
   xname <- rep(name,16)
@@ -46,101 +43,101 @@ staticPlot <- function(scData,selectNum){
   filterColums(makeTable(scData),selectNum)
 }
 
-staticPlot(df,15)
-
-
-
-# nameve <- names(df)
-# unuseable <- rep(0,16)
-# usable <-rep(0,16)
 # 
-# for(k in 1:16){
+# df <- read_csv2(config$scDataset, col_names = FALSE)
+# df <- cleanDataframe(df)
+# 
+# nameve <- names(df)
+# unuseable <- rep(0,17)
+# usable <-rep(0,17)
+#  
+# for(k in 1:17){
 #   unuseable[k]<- sum(is.na(df[,k]))
 #   usable[k] <- nrow(df) - unuseable[k]
 # }
 # 
 # show <- data.frame(column = nameve, usable = usable,unuseable= unuseable)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# miss going --------------------------------------------------------------
-
-
-makePivotTable <-function(df){
-  testDf <- df
-  testDf <- testDf %>% 
-    filter(!is.na(charged_kwh)) %>% 
-    mutate(start_date = sapply(start_date,function(x) { substr(x,1,7)}))
-  pivotTable <- dcast(testDf,user_id~start_date,value.var = "charged_kwh",sum)
-  return(pivotTable)
-}
-
-selectUser <- function(pivotTable,x){
-  a <- x
-  selectUser <- pivotTable %>% 
-    filter(user_id == a) %>% 
-    mutate(user_id = as.character(user_id))
-  changeStructure <- gather(selectUser,year,kwh,-user_id)
-  return(changeStructure)
-}
-
-makePlot <- function(changeStructure){
-  p <- ggplot(data=changeStructure, aes(x= year, y=kwh)) +
-    geom_line(stat = 'summary', fun.y=sum) +
-    stat_summary(fun.y =sum,geom ="line") +
-    geom_point() 
-  
-  p <- ggplotly(p)
-  return(p)
-}
-
-pivotTable <- makePivotTable(df)
-changeStructure <- selectUser(pivotTable,2259)
-makePlot(changeStructure)
-
-
-finalplot <- function(scData,text){
-  makePlot(selectUser(makePivotTable(scData),text))
-}
-
-finalplot(df,2259)
-
-# p <- ggplot(data=changeStructure, aes(x= year, y=kwh)) +
-#   geom_line(stat = 'summary', fun.y=sum) +
-#   stat_summary(fun.y =sum,geom ="line") +
-#   geom_point()
 # 
-# p <- ggplotly(p)
-# p
-
-sary <- summary(pivotTable)
-head(sary)
-sary[4, ]
+# 
 
 
 
 
 
-ss <- gather(sary[4,],year,mean,-user_id)
 
-dim(sary)
-head(sary)
-class(sary[4, ])
-sary[4]
+
+
+
+
+
+
+
+
+# 
+# 
+# 
+# 
+# # miss going --------------------------------------------------------------
+# 
+# 
+# makePivotTable <-function(df){
+#   testDf <- df
+#   testDf <- testDf %>% 
+#     filter(!is.na(charged_kwh)) %>% 
+#     mutate(start_date = sapply(start_date,function(x) { substr(x,1,7)}))
+#   pivotTable <- dcast(testDf,user_id~start_date,value.var = "charged_kwh",sum)
+#   return(pivotTable)
+# }
+# 
+# selectUser <- function(pivotTablse,x){
+#   a <- x
+#   selectUser <- pivotTable %>% 
+#     filter(user_id == a) %>% 
+#     mutate(user_id = as.character(user_id))
+#   changeStructure <- gather(selectUser,year,kwh,-user_id)
+#   return(changeStructure)
+# }
+# 
+# makePlot <- function(changeStructure){
+#   p <- ggplot(data=changeStructure, aes(x= year, y=kwh)) +
+#     geom_line(stat = 'summary', fun.y=sum) +
+#     stat_summary(fun.y =sum,geom ="line") +
+#     geom_point() 
+#   
+#   p <- ggplotly(p)
+#   return(p)
+# }
+# 
+# pivotTable <- makePivotTable(df)
+# changeStructure <- selectUser(pivotTable,2259)
+# makePlot(changeStructure)
+# 
+# 
+# finalplot <- function(scData,text){
+#   makePlot(selectUser(makePivotTable(scData),text))
+# }
+# 
+# finalplot(df,2259)
+# 
+# # p <- ggplot(data=changeStructure, aes(x= year, y=kwh)) +
+# #   geom_line(stat = 'summary', fun.y=sum) +
+# #   stat_summary(fun.y =sum,geom ="line") +
+# #   geom_point()
+# # 
+# # p <- ggplotly(p)
+# # p
+# # 
+# # sary <- summary(pivotTable)
+# # head(sary)
+# # sary[4, ]
+# # 
+# # 
+# # 
+# # 
+# # 
+# # ss <- gather(sary[4,],year,mean,-user_id)
+# # 
+# # dim(sary)
+# # head(sary)
+# # class(sary[4, ])
+# # sary[4]
