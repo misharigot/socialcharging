e82e4138f3ec6f183da8786f44cc8fe247888cea
U-DESKTOP-JMKUVFS\Amin Chihab <amin_chihab@hotmail.com>
2017-11-22 22:35:02 +0100

    Changed station numbers to names

--- src/helpers/data_helper.R
+++ src/helpers/data_helper.R
@@ -20,9 +20,37 @@ cleanDf <- prepareDataForStationPred(result)
 sessionsIdsWithPreds <- createLinearModelDataStation(cleanDf)
 result <- base::merge(cleanDf, sessionsIdsWithPreds, by = "session_id")
 
+# Change numeric user classifications to descriptive names
+result$user_class <- as.character(result$user_class)
+result$user_class <- lapply(result$user_class, changeToDescriptiveName)
+
 # Writes dataframe to csv file
 result <- as.data.frame(lapply(result, unlist))
 write.csv(result, config$dataFolder)
 
+
+# Helper functions ----------------------------------------------------
+
+changeToDescriptiveName <- function(x) {
+  res <- switch(x,
+                "1" = "MorningMorning",
+                "2" = "MorningAfternoon",
+                "3" = "MorningEvening",
+                "4" = "MorningNight",
+                "5" = "AfternoonMorning",
+                "6" = "AfternoonAfternoon",
+                "7" = "AfternoonEvening",
+                "8" = "AfternoonNight",
+                "9" = "EveningMorning",
+                "10" = "EveningAfternoon",
+                "11" = "EveningEvening",
+                "12" = "EveningNight",
+                "13" = "NightMorning",
+                "14" = "NightAfternoon",
+                "15" = "NightEvening",
+                "16" = "NightNight",
+                "-1" = "Longer24Hours")
+  return(res)
+}
 
 
