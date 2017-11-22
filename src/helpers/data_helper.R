7e12180634b7db6df17ec159c33774c5855fbbdc
U-DESKTOP-JMKUVFS\Amin Chihab <amin_chihab@hotmail.com>
2017-11-22 01:08:10 +0100

    Added untracked files

--- src/helpers/data_helper.R
+++ src/helpers/data_helper.R
@@ -10,11 +10,17 @@ library(corrplot)
 config <- config::get(file = "config.yml")
 source(config$baseClean)
 source(config$multiplotHelper)
-source("./src/models/regression_profile.R")
+source("./src/models/regression_user_class.R")
 
 df <- read_csv2(config$scDataset, col_names = FALSE)
-
 df <- cleanSecondDf(df)
+
+# Creates a csv file with predictions based on user classification 
 cleanDf <- prepareDataForLM(df, 0)
-idsWithPreds <- createLinearModelData(cleanDf, FALSE)
-result <- base::merge(cleanDf, idsWithPreds, by = "session_id")
+sessionsIdsWithPreds <- createLinearModelData(cleanDf)
+result <- base::merge(cleanDf, sessionsIdsWithPreds, by = "session_id")
+
+write.csv(result, config$dataFolder, row.names = TRUE)
+
+
+
