507b55ba43051d30f08a55bb5ef74217a9e736d7
misharigot <misharigot@gmail.com>
2017-12-18 22:12:54 +0100

    Date helper functions are done

--- src/helpers/date_helper.R
+++ src/helpers/date_helper.R
@@ -13,31 +13,36 @@ nextWeekday <- function(wday) {
   }
 }
 
-# Returns the date of the 
-toNextWeekStartDate <- function(hours, day) {
+# Returns the start date time of a session, based on the starting hour and the day (1-7).
+toNextWeekStartDate <- function(startingHour, day) {
   nextMonday <- nextWeekday(1)
   date <- nextMonday + day - 1
-  hms <- hms(paste0(hours, ":00:00"))
+  hms <- hms(paste0(startingHour, ":00:00"))
   ymd_hms(paste(date, hms, sep = "-"))
 }
 
-# ToDo: Finish this function, converting prep_start_time, day and pred_hours_elapsed to an actual date for next week
-toNextWeekEndDate <- function(hours, day, elapsed) {
-  # Return the end date as next week's date
-  nextMonday <- nextWeekday(1)
-  if (hours + elapsed > 24) {
-    date <- nextMonday + day
-    hours <- elapsed - (24 - hours)
-  }else {
-    date <- nextMonday + day - 1
-    hours <- hours + elapsed
+# Returns the end date time of a session, based on the starting hour, the day (1-7) and the time elapsed (hour decimal).
+toNextWeekEndDate <- function(startingHour, day, elapsed) {
+  startDate <- toNextWeekStartDate(startingHour, day)
+  endDate <- startDate + startingHour(floor(elapsed)) + minutes(getMinutes(elapsed))
+}
+
+# Returns the minutes of a decimal hour (hours = 1.1 returns 6 minutes)
+getMinutes <- function(hours) {
+  if (isDecimal(hours)) {
+    minutesFraction <- hours - floor(hours)
+    result <- minutesFraction * 60
+    return(floor(result))
+  } else {
+    return(0)
   }
-  hms <- hms(paste0(hours, ":00:00"))
-  ymd_hms(paste(date, hms, sep = "-"))
+}
+
+# Check if a number is a decimal number
+isDecimal <- function(number) {
+  number %% 1 != 0
 }
 
 toShowKwh <- function(kwh){
   paste0(kwh," kwh")
 }
-
-
