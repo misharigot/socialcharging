667c3391ab7e7a9273f055e18bf8b15d4963f1aa
misharigot <misharigot@gmail.com>
2018-01-20 22:07:53 +0100

    Predicting starting_hour works, but needs refinement and accuracies

--- src/models/regression_per_user.R
+++ src/models/regression_per_user.R
@@ -20,11 +20,12 @@ filterSessions <- function(df) {
 
 # Adds features (new columns) based on existing columns in df.
 addFeatures <- function(df) {
-  day <- factor(seq(1,7))
-  df %>% mutate(starting_hour = as.factor(paste0(hour(start_date), ".", ifelse(minute(start_date) < 30, 0, 5)) ),
-                day = as.factor(getDay(start_date)),
-                weekend = as.logical(ifelse(day %in% c(6,7), 1, 0))
-                )
+  df %>%
+    mutate(
+      starting_hour = factor(as.double(paste0(hour(start_date), ".", ifelse(minute(start_date) < 30, "0", "5")), levels = seq(0, 23.5, by = .5))),
+      day = factor(getDay(start_date), levels = seq(1, 7)),
+      weekend = as.logical(ifelse(day %in% c(6, 7), 1, 0))
+    )
 }
 
 # Returns sessions where users have minimumSessions amount of sessions
@@ -44,8 +45,8 @@ getSessions <- function(minimumSessions = 30) {
 }
 
 sessions <- getSessions(minimumSessions = 30)
-
-sessionsForUser <- sessions %>% filter(user_id == 1080)
+summary(sessions)
+sessionsForUser <- sessions %>% filter(user_id == 46)
 # sessionsForUser <- sessions %>% filter(user_id == 46) %>% filter(start_date >=  "2017-01-09", end_date <= "2017-01-17")
 
 summForUser <- sessionsForUser %>% group_by(day, starting_hour) %>% summarise(count = n())
@@ -56,7 +57,6 @@ summForUser <- sessionsForUser %>% group_by(day, starting_hour) %>% summarise(co
 #   hour = c(seq(0,23), seq(0,23), seq(0,23), seq(0,23), seq(0,23), seq(0,23), seq(0,23))
 # )
 
-
 getWeeksElapsed <- function(date1, date2) {
   as.numeric(round(abs(date1 - date2) / 7, 0))
 }
@@ -64,20 +64,20 @@ getWeeksElapsed <- function(date1, date2) {
 # 1. Predict the chance a session is going to happen on day x for user y.
 # 2. For days with > 0 sessions, predict their starting_hour
 
-# sessionsOnMonday / totalMondays
-
-# classifier <- naiveBayes(sessionsForUser, sessionsForUser$starting_hour, formula = starting_hour ~ day, laplace = 1)
-
 predictWeekForUser <- function(userId, sessions) {
-  userId = 1080
+  # userId = 1080
   sessions <- sessions %>% filter(user_id == userId)
   
-  classifier <- naiveBayes(starting_hour ~ day + weekend, sessions, laplace = 1)
+  if (nrow(sessions) == 0) {
+    warning("sessions df is empty.")
+  }
+  
+  classifier <- naive_bayes(starting_hour ~ day + weekend, sessions, laplace = 1)
   
   weekDf <- data.frame(
     user_id = rep(userId, 7),
-    day = c(seq(1,7)),
-    weekend = c(0,0,0,0,0,1,1)
+    day = factor(seq(1, 7)),
+    weekend = as.logical(c(0, 0, 0, 0, 0, 1, 1))
   )
   
   sessionsPerDay <- sessions %>%
@@ -89,23 +89,35 @@ predictWeekForUser <- function(userId, sessions) {
     summarise(min = min(start_date), max = max(start_date), totalWeeksElapsed = getWeeksElapsed(min, max))
   
   sessionsPerDay <- base::merge(sessionsPerDay, weeksElapsed[, c("user_id", "totalWeeksElapsed")], by = "user_id") %>% 
-    mutate(avgSessions = amountOfSessions/totalWeeksElapsed) %>%
+    mutate(sessionChance = totalWeeksElapsed / amountOfSessions * 100) %>%
     filter(user_id == userId)
-  ?naiveBayes
-  pred <- predict(classifier, weekDf, type = "class")
-  weekDf$pred <- predict(classifier, weekDf, type = "class")
-  # weekDf <- base::merge(weekDf, sessionsPerDay[, c("day", "avgSessions")], by = "day")
+  
+  occuringDays <- sessions %>% distinct(day)
+  filteredWeekDf <- weekDf %>% filter(day %in% occuringDays$day) %>% mutate(day = factor(day, levels = occuringDays$day))
+  filteredWeekDf$pred_starting_hour <- predict(classifier, filteredWeekDf, type = "class")
+  
+  # To Do: get this prob in a column
+  filteredWeekDf$pred_acc <- 0
+  probability <- predict(classifier, filteredWeekDf, type = "prob")
+
+  nonOccuringDays <- weekDf %>% filter(!(day %in% occuringDays$day))
+  if (nrow(nonOccuringDays) > 0) {
+    nonOccuringDays$pred_starting_hour <- NA
+    nonOccuringDays$pred_acc <- NA
+    filteredWeekDf <- rbind(filteredWeekDf, nonOccuringDays) 
+  }
+  resultWeekDf <- base::merge(filteredWeekDf, sessionsPerDay[, c("day", "sessionChance")], by = "day", all = T)
 }
 
-predicted <- predictWeekForUser(1080, sessions)
+predicted <- predictWeekForUser(184, sessions)
 
 # Accuracy
-truth <- (as.double(as.character(pred)) -  as.double(as.character(sessionsForUser$starting_hour)) < .5)
-table(truth)
-trues <- sum(table(truth)["TRUE"])
-falses <- sum(table(truth)["FALSE"])
-total <- trues + falses
-trues / total
+# truth <- (as.double(as.character(pred)) -  as.double(as.character(sessionsForUser$starting_hour)) < .5)
+# table(truth)
+# trues <- sum(table(truth)["TRUE"])
+# falses <- sum(table(truth)["FALSE"])
+# total <- trues + falses
+# trues / total
 
 # Returns the sessions belonging to a random week in the df given.
 getRandomWeekData <- function(df) {
